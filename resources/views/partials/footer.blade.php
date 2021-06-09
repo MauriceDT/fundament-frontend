25cebeb19239eea51908e5af9b29bf18388089a1
Maurice di Tolve <maurice_di_tolve@hotmail.com>
2021-06-09 15:49:33 +0200

    09/06/2021 part2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
-    <div class="socials">
+    <div class="footer-socials">
         <img class="img-facebook" src="/images/facebook-logo.svg" alt="Facebook Logo">
-        <img class="img-twitter"src="/images/twitter.png" alt="Facebook Logo">
+        <img class="img-twitter"src="/images/twitter.svg" alt="Twitter Logo">
     </div>
     <div class="footer-text">
         Powered by Fundament All Media
